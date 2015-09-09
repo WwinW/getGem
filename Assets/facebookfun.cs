0cac344b2b6a72b38ab4b239c3fcb0be7d3d78c3
weiwei <weiwei@blingstorm.com.cn>
2015-09-09 18:52:31 +0800

    删除poolmanager

--- Assets/facebookfun.cs
+++ Assets/facebookfun.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Facebook : MonoBehaviour {
+public class facebookfun : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
